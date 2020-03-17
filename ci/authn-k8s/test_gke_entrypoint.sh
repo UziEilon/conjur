9f9237f3545615c1dce15ddab82f752aab682565
Jonah <jonahx@gmail.com>
2020-03-17 12:56:46 -0400

    GOOD: Cleanup: Make shellcheck pass

--- ci/authn-k8s/test_gke_entrypoint.sh
+++ ci/authn-k8s/test_gke_entrypoint.sh
@@ -2,18 +2,24 @@
 
 set -o pipefail
 
-# expects
-# GCLOUD_CLUSTER_NAME GCLOUD_ZONE GCLOUD_PROJECT_NAME GCLOUD_SERVICE_KEY
-# CONJUR_AUTHN_K8S_TEST_NAMESPACE CONJUR_AUTHN_K8S_TAG INVENTORY_TAG
+# Expects the following to be defined:
+#
+# GCLOUD_CLUSTER_NAME
+# GCLOUD_ZONE
+# GCLOUD_PROJECT_NAME
+# GCLOUD_SERVICE_KEY
+# CONJUR_AUTHN_K8S_TEST_NAMESPACE
+# CONJUR_AUTHN_K8S_TAG
+# INVENTORY_TAG
 # LOCAL_DEV_VOLUME
-# to exist
-
 # PWD = /src (which is mapped via docker volume to ${WORKSPACE}/ci/authn-k8s)
 
-export LOCAL_DEV_VOLUME=$(cat <<- ENDOFLINE
-emptyDir: {}
-ENDOFLINE
-)
+
+# LOCAL_DEV_VOLUME=$(cat <<- ENDOFLINE
+# emptyDir: {}
+# ENDOFLINE
+# )
+export LOCAL_DEV_VOLUME='emptyDir: {}'
 
 function finish {
   echo 'Finishing'
@@ -24,13 +30,15 @@ function finish {
       echo "Grabbing output from $pod_name"
       echo '-----'
       mkdir -p output
-      kubectl cp $pod_name:/opt/conjur-server/output output
+      kubectl cp "$pod_name:/opt/conjur-server/output" output
 
       echo "Logs from Conjur Pod $pod_name:"
-      kubectl logs $pod_name > output/gke-authn-k8s-logs.txt
+      kubectl logs "$pod_name" > output/gke-authn-k8s-logs.txt
 
       # Rails.logger writes the logs to the environment log file
-      kubectl exec $pod_name -- bash -c "cat /opt/conjur-server/log/test.log" >> output/gke-authn-k8s-logs.txt
+      kubectl exec "$pod_name" -- \
+        bash -c "cat /opt/conjur-server/log/test.log" >> \
+        output/gke-authn-k8s-logs.txt
 
       echo "Printing Logs from Conjur to the console"
       echo "==========================="
@@ -40,24 +48,27 @@ function finish {
       echo "Killing conjur so that coverage report is written"
       # The container is kept alive using an infinite sleep in the at_exit hook
       # (see .simplecov) so that the kubectl cp below works.
-      kubectl exec $pod_name -- bash -c "pkill -f 'puma 3'"
+      kubectl exec "$pod_name" -- bash -c "pkill -f 'puma 3'"
 
       echo "Retrieving coverage report"
-      kubectl cp $pod_name:/opt/conjur-server/coverage/.resultset.json output/simplecov-resultset-authnk8s-gke.json
+      kubectl cp "$pod_name:/opt/conjur-server/coverage/.resultset.json" \
+        output/simplecov-resultset-authnk8s-gke.json
     fi
   } || {
     echo "Logs could not be extracted from $pod_name"
-    touch output/gke-authn-k8s-logs.txt  # so Jenkins artifact collection doesn't fail
+    # so Jenkins artifact collection doesn't fail
+    touch output/gke-authn-k8s-logs.txt
   }
 
-  echo 'Removing namespace $CONJUR_AUTHN_K8S_TEST_NAMESPACE'
+  echo "Removing namespace $CONJUR_AUTHN_K8S_TEST_NAMESPACE"
   echo '-----'
-  kubectl --ignore-not-found=true delete namespace $CONJUR_AUTHN_K8S_TEST_NAMESPACE
+  kubectl --ignore-not-found=true delete namespace \
+    "$CONJUR_AUTHN_K8S_TEST_NAMESPACE"
 
-  delete_image $CONJUR_TEST_AUTHN_K8S_TAG
-  delete_image $CONJUR_AUTHN_K8S_TAG
-  delete_image $INVENTORY_TAG
-  delete_image $NGINX_TAG
+  delete_image "$CONJUR_TEST_AUTHN_K8S_TAG"
+  delete_image "$CONJUR_AUTHN_K8S_TAG"
+  delete_image "$INVENTORY_TAG"
+  delete_image "$NGINX_TAG"
 }
 trap finish EXIT
 
@@ -98,75 +109,99 @@ function renderResourceTemplates() {
 
 function createNamespace() {
   # clean ups namespaces older than minutes or seconds
-  old_namespaces=$(kubectl get namespaces | awk '$1 ~ /test-/ && $3 !~ /[m|s]/ { print $1; }')
-  [ ! -z "${old_namespaces}" ] && kubectl delete --ignore-not-found=true namespaces ${old_namespaces}
+  old_namespaces=$(kubectl get namespaces | \
+    awk '$1 ~ /test-/ && $3 !~ /[m|s]/ { print $1; }')
+  [ ! -z "${old_namespaces}" ] && \
+    kubectl delete --ignore-not-found=true namespaces "${old_namespaces}"
 
-  kubectl create namespace $CONJUR_AUTHN_K8S_TEST_NAMESPACE
-  kubectl config set-context $(kubectl config current-context) --namespace=$CONJUR_AUTHN_K8S_TEST_NAMESPACE
+  kubectl create namespace "$CONJUR_AUTHN_K8S_TEST_NAMESPACE"
+  kubectl config set-context "$(kubectl config current-context)" \
+    --namespace="$CONJUR_AUTHN_K8S_TEST_NAMESPACE"
 
   # Grant default service account permissions it needs for authn-k8s to:
   # 1) get + list various resources
   # 2) create + get pods/exec (to inject cert into app sidecar)
   kubectl apply -f dev/dev_conjur_authenticator_role.${TEMPLATE_TAG}yaml
   wait_for_it 1 "kubectl get clusterrole conjur-authenticator"
-  kubectl create -f dev/dev_conjur_authenticator_role_binding.${TEMPLATE_TAG}yaml
+  kubectl create --filename dev/dev_conjur_authenticator_role_binding.${TEMPLATE_TAG}yaml
 }
 
 function pushDockerImages() {
-  gcloud docker -- push $CONJUR_AUTHN_K8S_TAG
-  gcloud docker -- push $CONJUR_TEST_AUTHN_K8S_TAG
-  gcloud docker -- push $INVENTORY_TAG
-  gcloud docker -- push $NGINX_TAG
+  gcloud docker -- push "$CONJUR_AUTHN_K8S_TAG"
+  gcloud docker -- push "$CONJUR_TEST_AUTHN_K8S_TAG"
+  gcloud docker -- push "$INVENTORY_TAG"
+  gcloud docker -- push "$NGINX_TAG"
 }
 
 function launchConjurMaster() {
   echo 'Launching Conjur master service'
 
-  sed -e "s#{{ CONJUR_AUTHN_K8S_TAG }}#$CONJUR_AUTHN_K8S_TAG#g" dev/dev_conjur.${TEMPLATE_TAG}yaml |
-    sed -e "s#{{ CONJUR_TEST_AUTHN_K8S_TAG }}#$CONJUR_TEST_AUTHN_K8S_TAG#g" |
-    sed -e "s#{{ DATA_KEY }}#$(openssl rand -base64 32)#g" |
-    sed -e "s#{{ CONJUR_AUTHN_K8S_TEST_NAMESPACE }}#$CONJUR_AUTHN_K8S_TEST_NAMESPACE#g" |
-    kubectl create -f -
+  sed -e "s#{{ CONJUR_AUTHN_K8S_TAG }}#$CONJUR_AUTHN_K8S_TAG#g" \
+    dev/dev_conjur.${TEMPLATE_TAG}yaml |
+  sed -e "s#{{ CONJUR_TEST_AUTHN_K8S_TAG }}#$CONJUR_TEST_AUTHN_K8S_TAG#g" |
+  sed -e "s#{{ DATA_KEY }}#$(openssl rand -base64 32)#g" |
+  sed -e "s#{{ CONJUR_AUTHN_K8S_TEST_NAMESPACE }}#$CONJUR_AUTHN_K8S_TEST_NAMESPACE#g" |
+  kubectl create --filename -
 
-  conjur_pod=$(kubectl get pods -l app=conjur-authn-k8s -o=jsonpath='{.items[].metadata.name}')
+  conjur_pod=$(kubectl get pods \
+    --selector app=conjur-authn-k8s \
+    --output=jsonpath='{.items[].metadata.name}')
 
-  wait_for_it 300 "kubectl describe po $conjur_pod | grep Status: | grep -q Running"
+  # TODO: Refactor this -- passing bash code to be eval'd by a function
+  #       isn't good practice.
+  wait_for_it 300 "kubectl describe po $conjur_pod | \
+    grep Status: | grep -q Running"
 
   # wait for the 'conjurctl server' entrypoint to finish
-  kubectl exec $conjur_pod -- bash -c "while ! curl -sI localhost:80 > /dev/null; do sleep 1; done"
+  kubectl exec "$conjur_pod" -- bash -c "while ! curl -sI localhost:80 > \
+    /dev/null; do sleep 1; done"
   
-  export API_KEY=$(kubectl exec $conjur_pod -- conjurctl account create cucumber | tail -n 1 | awk '{ print $NF }')
+  API_KEY=$(kubectl exec "$conjur_pod" -- \
+    conjurctl account create cucumber | \
+    tail -n 1 | \
+    awk '{ print $NF }')
+  export API_KEY
 }
 
 function copyNginxSSLCert() {
-  nginx_pod=$(kubectl get pods -l app=nginx-authn-k8s -o=jsonpath='{.items[].metadata.name}')
-  cucumber_pod=$(kubectl get pods -l app=cucumber-authn-k8s -o=jsonpath='{.items[].metadata.name}')
+  nginx_pod=$(kubectl get pods --selector app=nginx-authn-k8s \
+    --output=jsonpath='{.items[].metadata.name}')
+  cucumber_pod=$(kubectl get pods --selector app=cucumber-authn-k8s \
+    --output=jsonpath='{.items[].metadata.name}')
 
-  kubectl cp $nginx_pod:/etc/nginx/nginx.crt ./nginx.crt
-  kubectl cp ./nginx.crt $cucumber_pod:/opt/conjur-server/nginx.crt
+  kubectl cp "$nginx_pod:/etc/nginx/nginx.crt" ./nginx.crt
+  kubectl cp ./nginx.crt "$cucumber_pod:/opt/conjur-server/nginx.crt"
 }
 
 function copyConjurPolicies() {
-  cli_pod=$(kubectl get pod -l app=conjur-cli --no-headers | grep Running | awk '{ print $1 }')
+  cli_pod=$(kubectl get pod --selector app=conjur-cli --no-headers | \
+    grep Running | \
+    awk '{ print $1 }')
 
-  kubectl cp ./dev/policies $cli_pod:/policies
+  kubectl cp ./dev/policies "$cli_pod:/policies"
 }
 
 function loadConjurPolicies() {
   echo 'Loading the policies and data'
 
-  cli_pod=$(kubectl get pod -l app=conjur-cli --no-headers | grep Running | awk '{ print $1 }')
+  cli_pod=$(kubectl get pod --selector app=conjur-cli --no-headers | \
+    grep Running | \
+    awk '{ print $1 }')
   
-  kubectl exec $cli_pod -- conjur init -u conjur -a cucumber
+  kubectl exec "$cli_pod" -- conjur init -u conjur -a cucumber
   sleep 5
-  kubectl exec $cli_pod -- conjur authn login -u admin -p $API_KEY
+  kubectl exec "$cli_pod" -- conjur authn login -u admin -p "$API_KEY"
 
   # load policies
-  kubectl exec $cli_pod -- conjur policy load root /policies/policy.${TEMPLATE_TAG}yml
+  kubectl exec "$cli_pod" -- \
+    conjur policy load root "/policies/policy.${TEMPLATE_TAG}yml"
 
   # init ca certs
-  conjur_pod=$(kubectl get pod -l app=conjur-authn-k8s --no-headers | grep Running | awk '{ print $1 }')
-  kubectl exec $conjur_pod -- rake authn_k8s:ca_init["conjur/authn-k8s/minikube"]
+  conjur_pod=$(kubectl get pod --selector app=conjur-authn-k8s --no-headers | \
+    grep Running | awk '{ print $1 }')
+
+  kubectl exec "$conjur_pod" -- \
+    rake authn_k8s:ca_init["conjur/authn-k8s/minikube"]
 }
 
 function launchInventoryServices() {
@@ -177,7 +212,8 @@ function launchInventoryServices() {
   kubectl create -f dev/dev_inventory_pod.${TEMPLATE_TAG}yaml
   kubectl create -f dev/dev_inventory_unauthorized.${TEMPLATE_TAG}yaml
 
-  wait_for_it 300 "kubectl describe po inventory | grep Status: | grep -c Running | grep -q 4"
+  wait_for_it 300 "kubectl describe po inventory | \
+    grep Status: | grep -c Running | grep -q 4"
 }
 
 function runTests() {
